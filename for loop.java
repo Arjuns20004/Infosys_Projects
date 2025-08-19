6a77aafe79c5f04abd275ea1cb5b57d62d7f297d
Arjun Selvaraj <145590376+Arjuns20004@users.noreply.github.com>
2025-08-19 10:48:12 +0530

    Delete for loop.java

--- for loop.java
+++
@@ -1,9 +0,0 @@
-class Main
-{
-    public static void main(String args[])
-    {
-        args=new String[]{"Arjun","B.E","CSE","IoT"};
-        for(int i=0;i<args.length;i++)
-        System.out.println("Index :"+ i+" :"+args[i]);
-    }
-} 
