02f4d7c5e15f5cacff291caa764082ab0d13e0fc
Arjun Selvaraj <145590376+Arjuns20004@users.noreply.github.com>
2025-08-29 21:04:52 +0530

    Delete Day 8 directory

--- Day 8/Rightswift.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Rightswift {
-    public static void main(String[] args) {
-        Scanner lee=new Scanner(System.in);
-        System.out.println(-1>>1);
-    }    
-}
